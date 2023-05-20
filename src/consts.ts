8bc08010e16a02f7f614ec77cd2553fb7c2a339c
merore <merore256@outlook.com>
2023-05-20 17:43:35 +0800

    misc: lint site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Merore\'s blog';
+export const SITE_TITLE = 'Merore\'s Blog';
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
 export const SITE_OWNER = "merore";
